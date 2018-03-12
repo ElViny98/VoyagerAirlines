0cac7fd7d61df05f1366b9411f0579d361614802
ElViny98 <viny.mtz@gmail.com>
2018-03-12 16:40:57 -0700

    Clases principales
    Añadidas clases principales

---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,27 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexion {
+    public Connection abrirConexion() throws SQLException{
+        Connection con;
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Voyager", "root", "");
+        return con;
+    }
+    
+    public void cerrarConexion(Connection con) {
+        try {
+            if(!con.isClosed()) {
+                con.close();
+            }
+        } 
+        catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
